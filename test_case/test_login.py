9148e88de0716c1cfd331dc2cbbe0e94f35e170c
shaofeng666 <39985892+shaofeng666@users.noreply.github.com>
2019-01-18 18:23:16 +0800

    修改测试用例

--- test_case/test_login.py
+++ test_case/test_login.py
@@ -2,11 +2,11 @@ import requests
 import json
 import unittest
 from  util.modules import ddt
-from  config import root_path
+import  config
 from util.gettestdata import get_testcase
 from util.modules.HTMLTestRunner_API import set_response
 
-case_path = root_path + '\\data\\case.xlsx'
+case_path = config.root_path + '\\data\\case.xlsx'
 casedata = get_testcase(case_path, 1, '登录')
 
 
@@ -22,9 +22,10 @@ class requsetsTest(unittest.TestCase):
         asserts=casedata['asserts']
         header = {'Content-Type': 'application/json',
                   "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:51.0) Gecko/20100101 Firefox/51.0"}
-        r = requests.get('http://t.weather.sojson.com/api/weather/city/101030100', headers=header)
+        r = requests.get(config.hosts+'?'+url, headers=header)
         response=r.text
         set_response(response)
+        assert '200'==asserts
 
 
     def tearDown(self):
